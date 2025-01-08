a09f8aaaa4af6c17c7cb60ffc5deade4be394764
RubenvanGemeren <47777782+RubenvanGemeren@users.noreply.github.com>
2025-01-08 15:53:26 +0100

    add script

---
+++ fpl_project/restart_space.py
@@ -0,0 +1,15 @@
+from huggingface_hub import HfApi
+import os
+
+def restart_space():
+    token = os.environ['HF_KEY'] # Please navigate to Settings > Secrets and variables > Actions and define "HF_TOKEN".
+    repo_id = "ID2223JR/fpl_app" #  Please replace this value with the name of your own Hugging Face Space.
+
+    try:
+        HfApi().restart_space(repo_id=repo_id, token=token)
+        print(f"Successfully restarted Space: {repo_id}")
+    except Exception as e:
+        print(f"Failed to restart Space {repo_id}: {e}")
+
+if __name__ == "__main__":
+    restart_space()
